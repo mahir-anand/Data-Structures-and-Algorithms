dfd3ca9cf953b8510a01156604fe5cc5117a71d4
mahir118 <mahiranand11@gmail.com>
2023-01-19 17:14:43 -0800

    Permutation of a String - Backtracking

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -2,13 +2,19 @@ public class JavaBasics {
 
     public static void permutation (String str, String ans) { // "abc" , ""
         //base case
-
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
         //recursion
-        newStr = str.substring(0,i) + str.substring(i+1);
-        permutation(newStr, ans+str.charAt(i));
+        for (int i = 0 ; i < str.length() ; i++) { 
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            permutation(newStr, ans + str.charAt(i));
+        }
     }
     public static void main (String args[]) {
-
+        String str = "abc";
+        permutation(str, "");
     }
 
 }
