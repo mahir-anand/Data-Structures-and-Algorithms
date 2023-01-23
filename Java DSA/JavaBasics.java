3f356d714688d39a37eb52c03fac89028e428203
mahir118 <mahiranand11@gmail.com>
2023-01-23 14:32:27 -0800

    Unique Path in a Grid

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -1,68 +1,20 @@
 public class JavaBasics {
-
-    public static boolean nQueens (char board[][], int row) {
-        //base case
-        if (row == board.length) {
-            printBoard(board);
-            // count++;
-            return true;
+    
+    public static int gridWays (int i, int j, int n, int m) {
+        // base case
+        if (i == n-1 && j ==m-1) {
+            return 1;
+        } else if (i == n || j == m) {
+            return 0;
         }
         //recursion
-        for (int j = 0 ; j < board.length ; j++) {
-            if (isSafe (board,row,j)) {
-                board [row][j] = 'Q';
-                if (nQueens(board, row+1)) {
-                    return true;
-                }
-            }
-            board[row][j] = '.';
-        }
-        return false;
+        int w1 = gridWays(i+1, j, n, m); //down
+        int w2 = gridWays(i, j+1, n, m); //right
+        return w1 + w2;
     }
-
-    public static boolean isSafe (char board[][], int row, int col) {
-        // vertically up
-        for (int i = row - 1 ; i >=0 ; i--) {
-            if (board[i][col] == 'Q') {
-                return false;
-            }
-        }
-        // diagonally left
-        for (int i = row -1 , j = col - 1 ; i >= 0 && j >=0 ; i--,j--) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-        // diagonally right
-        for (int i = row -1 , j = col + 1 ; i >= 0 && j < board.length ; i--,j++) {
-            if (board[i][j] == 'Q') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void printBoard (char board[][]) {
-        System.out.println("---------chess board---------");
-        for (int i = 0 ; i < board.length ; i++) {
-            for (int j = 0 ; j < board.length ; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    static int count = 0;
     public static void main (String args[]) {
-        //initializing board
-        int n = 5;
-        char board[][] = new char[n][n];
-        for (int i = 0 ; i < n ; i++) {
-            for (int j = 0 ; j < n ; j++) {
-                board [i][j] = '.';
-            }
-        }
-        nQueens(board, 0);
+        int n = 4, m = 4;
+        System.out.println(gridWays(0, 0, n, m));
     }
 
 }
