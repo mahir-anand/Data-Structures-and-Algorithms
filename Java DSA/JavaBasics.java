d0482c9a2a66bb0aed62aac014196d9379e97f4f
mahir118 <mahiranand11@gmail.com>
2023-01-23 14:51:10 -0800

    Unique paths in linear time

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -2,15 +2,28 @@ public class JavaBasics {
     
     public static int gridWays (int i, int j, int n, int m) {
         // base case
-        if (i == n-1 && j ==m-1) {
+        // if (i == n-1 && j == m-1) {
+        //     return 1;
+        // } else if (i == n || j == m) {
+        //     return 0;
+        // }
+        // //recursion
+        // int w1 = gridWays(i+1, j, n, m); //down
+        // int w2 = gridWays(i, j+1, n, m); //right
+        // return w1 + w2;
+        int num = fact (n - 1 + m - 1);
+        int den = fact (n-1) * fact (m-1);
+        return num/den;
+    }
+
+    public static int fact ( int num ) {
+        //base case
+        if (num == 1) {
             return 1;
-        } else if (i == n || j == m) {
-            return 0;
         }
         //recursion
-        int w1 = gridWays(i+1, j, n, m); //down
-        int w2 = gridWays(i, j+1, n, m); //right
-        return w1 + w2;
+        int fact = num * fact (num-1);
+        return fact;
     }
     public static void main (String args[]) {
         int n = 4, m = 4;
