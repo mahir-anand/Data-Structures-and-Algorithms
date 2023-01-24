d037c745570a298e900a1888bab8eb096e76888d
mahir118 <mahiranand11@gmail.com>
2023-01-24 13:36:22 -0800

    Sudoku Solver

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -1,33 +1,15 @@
 public class JavaBasics {
-    
-    public static int gridWays (int i, int j, int n, int m) {
-        // base case
-        // if (i == n-1 && j == m-1) {
-        //     return 1;
-        // } else if (i == n || j == m) {
-        //     return 0;
-        // }
-        // //recursion
-        // int w1 = gridWays(i+1, j, n, m); //down
-        // int w2 = gridWays(i, j+1, n, m); //right
-        // return w1 + w2;
-        int num = fact (n - 1 + m - 1);
-        int den = fact (n-1) * fact (m-1);
-        return num/den;
-    }
 
-    public static int fact ( int num ) {
-        //base case
-        if (num == 1) {
-            return 1;
-        }
-        //recursion
-        int fact = num * fact (num-1);
-        return fact;
-    }
     public static void main (String args[]) {
-        int n = 4, m = 4;
-        System.out.println(gridWays(0, 0, n, m));
+        int sodoku[][] = { {0,0,8,0,0,0,0,0,0} ,
+        {4,9,0,1,5,7,0,0,2},
+        {0,0,3,0,0,4,1,9,0},
+        {1,8,5,0,6,0,0,2,0},
+        {0,0,0,0,2,0,0,6,0,},
+        {9,6,0,4,0,5,3,0,0},
+        {0,3,0,0,7,2,0,0,4},
+        {0,4,9,0,3,0,0,5,7},
+        {8,2,7,0,0,9,0,1,3} };
     }
 
 }
