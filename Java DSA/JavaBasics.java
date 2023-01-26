92985aa3f5691d7a1c07e955ca0c45b86ef9eca2
mahir118 <mahiranand11@gmail.com>
2023-01-26 11:15:38 -0800

    Sodoku Solver with Backtracking

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -2,13 +2,11 @@ public class JavaBasics {
 
     public static boolean sodokuSolver (int sodoku[][], int row, int col) {
         //base case
-        if (row == 8 && col == 8) {
+        if (row > 8) {
             return true;
-        } else if (row == 8) {
-            return false;
         }
-        //recursion
 
+        //recursion
         int nextRow = row, nextCol = col + 1;
         if (nextCol > 8) {
             nextRow++;
@@ -29,6 +27,45 @@ public class JavaBasics {
         }
         return false;
     }
+
+    public static boolean isSafe (int sodoku[][], int row, int col, int num) {
+        //row
+        for (int j = 0 ; j < sodoku.length ; j++) {
+            if (sodoku[row][j] == num) {
+                return false;
+            }
+        }
+
+        //col
+        for (int i = 0 ; i < sodoku.length ; i++) {
+            if (sodoku[i][col] == num) {
+                return false;
+            }
+        }
+
+        //grid
+        int startRow = (row/3) * 3;
+        int startCol = (col/3) * 3;
+        for (int i = 0 ; i < startRow + 3 ; i++) {
+            for (int j = 0 ; j < startCol + 3 ; j++) {
+                if (sodoku[startRow][startCol] == num) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+
+    public static void printSodoku(int sodoku[][]) {
+        for (int i = 0 ; i < 9 ; i ++) {
+            for (int j = 0 ; j < 9 ; j++) {
+                System.out.print(sodoku[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
     public static void main (String args[]) {
         int sodoku[][] = { {0,0,8,0,0,0,0,0,0} ,
         {4,9,0,1,5,7,0,0,2},
@@ -39,6 +76,12 @@ public class JavaBasics {
         {0,3,0,0,7,2,0,0,4},
         {0,4,9,0,3,0,0,5,7},
         {8,2,7,0,0,9,0,1,3} };
-    }
+    
 
+        if (sodokuSolver (sodoku,0,0)) {
+            printSodoku(sodoku);
+        } else {
+            System.out.println("Solution doesn't exist");
+        }
+    }
 }
