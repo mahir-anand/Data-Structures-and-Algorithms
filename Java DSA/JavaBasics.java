cbbcb2beced22f93ec33b74785615aaef893fd55
mahir118 <mahiranand11@gmail.com>
2023-01-26 21:45:49 -0800

    Unique Paths

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -1,21 +1,20 @@
 public class JavaBasics {
 
-   public static void permutation (String str, String perString) { 
+    public static int uniquePath(int n, int m, int row, int col) {
         //base case
-        if (str.length() == 0) {
-            System.out.println(perString);
-            return;
+        if (row == n-1 && col == m-1) {
+            return 1;
+        } else if (row == n || col == m) {
+            return 0;
         }
         //recursion
-        for (int i = 0 ; i < str.length() ; i++) {
-            // permutation(str.substring(0, i) + str.substring(i + 1), perString + str.charAt(i));
-            // permutation(str.substring(0, i) + str.substring(i + 1),perString);
-            String newStr = str.substring(0,i) + str.substring(i+1);
-            permutation(newStr, perString + str.charAt(i));
-        }
-   }
-    public static void main (String args[]) {
-        String str = "abc";
-        permutation(str, "");
+        int down = uniquePath(n, m, row+1, col); // down
+        int right = uniquePath(n, m, row, col+1); // right
+        return down + right;
     }
+    public static void main (String args[]) {
+        int n = 3, m = 5;
+        System.out.println(uniquePath(n, m, 0, 0));
+    }
+    
 }
