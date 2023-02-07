7463a341bba37b2d7f03013d16df9e77d8704c7a
mahir118 <mahiranand11@gmail.com>
2023-02-07 08:35:19 -0800

    Optimized Kadane's Algorithm

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -2,15 +2,25 @@ public class JavaBasics {
 
     public static int maxSum (int arr[]) {
         int sum = 0;
+        int negCount = 0;
+        int maxNeg = Integer.MIN_VALUE;
         int maxSum = Integer.MIN_VALUE;
         for (int i = 0 ; i < arr.length ; i++) {
+            if (arr[i] < 0) {
+                negCount++;
+                maxNeg = Math.max(maxNeg,arr[i]);
+            }
             sum += arr [i];
             if (sum < 0) {
                 sum = 0;
             }
             maxSum = Math.max(sum,maxSum);
+
         }
 
+        if (negCount == arr.length) {
+            return maxNeg;
+        }
         return maxSum;
     }
     
