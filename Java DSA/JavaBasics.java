55038c5ccc36908e3066b50a1b802155aff64e81
mahir118 <mahiranand11@gmail.com>
2023-02-09 09:34:26 -0800

    Searching a Matrix

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -1,23 +1,26 @@
 public class JavaBasics {
 
-  public static int countDiagonal(int matrix[][]) {
-    int count = 0;
-    for (int i = 0 ; i < matrix.length ; i++) {
-        if (i != matrix.length - 1 - i) {
-            count += matrix[i][matrix.length - 1 - i];
+    public static void search(int matrix[][], int key) {
+        int row = matrix.length - 1;
+        int col = 0;
+        while (row >= 0 && col < matrix[0].length) {
+            if (matrix[row][col] > key) {
+                row--;
+            } else if (matrix[row][col] < key) {
+                col++;
+            } else {
+                System.out.println("Row is : " + row);
+                System.out.println("Col is : " + col);
+                break;
+            }
         }
-        count += matrix[i][i];
-        
     }
-    return count;
-  }
   
     public static void main (String args[]) {
-        int matrix[][] = { {1,2,3,4,0},
-                           {5,6,7,8,0},
-                           {9,10,11,12,0},
-                           {13,14,15,16,0},
-                           {0,0,0,0,0}};
-        System.out.println(countDiagonal(matrix));
+        int matrix[][] = { {1,2,3,4},
+                           {5,6,7,8},
+                           {9,10,11,12},
+                           {13,14,15,16}};
+        search(matrix, 15);
     }
 }
