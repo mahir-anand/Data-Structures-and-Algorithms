99678c028df0d76ac422355328b766e9ab2fb25d
mahir118 <mahiranand11@gmail.com>
2023-02-12 08:31:45 -0800

    Second Minimum in an Array

--- Java DSA/JavaBasics.java
+++ Java DSA/JavaBasics.java
@@ -4,27 +4,31 @@ public class JavaBasics {
         
         int max = Integer.MIN_VALUE;
         int secMax = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int secMin = Integer.MAX_VALUE;
         int count = 0;
+
         for (int i = 0 ; i < n ; i ++) {
-            if (arr[i] > max) {
-                secMax = max;
-                max = arr[i];
+            if (arr[i] < min) {
+                secMin = min;
+                min = arr[i];
                 count++;
-            } else if (arr[i] > secMax && arr[i] != max) {
-                secMax = arr[i];
+            } else if (arr[i] < secMin && arr[i] != min) {
+                secMin = arr[i];
                 count++;
             }
         }
-       
-       if (count < 2) {
-           return -1;
-       } else {
-        return secMax;   
+        if (count < 2) {
+            return -1;
+        } else {
+            return secMin; 
+        }
+        
        }
         
-    }
+
     public static void main (String args[]) {
-        int num[] = {2,1,4,5,6,4,3};
+        int num[] = {2,2,2,2,2,2,2};
         System.out.println(print2largest(num, 7));
     }
 }
