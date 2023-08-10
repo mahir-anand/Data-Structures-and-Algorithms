b92fdd8dfc565cb8f1b4d57819b42c096635f59d
mahir118 <mahiranand11@gmail.com>
2023-08-10 18:22:23 +0530

    Selection Sort

---
+++ Java DSA/SelectionSort.java
@@ -0,0 +1,34 @@
+public class SelectionSort {
+
+    public static void selection (int arr[]) {
+
+        int key = 0;
+
+        while (key < arr.length - 1) {
+
+            int min = key;
+
+            for (int i = key ; i < arr.length ; i++) {
+                if (arr[i] < arr[min]) {
+                    min = i;
+                }
+            }
+
+            int temp = arr[key];
+            arr[key] = arr[min];
+            arr[min] = temp;
+
+            key++;
+
+        }
+       
+    }
+    public static void main (String args[]) {
+        int arr[] = {56,4,-1,4,0,5};
+        selection(arr);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+    
+}
