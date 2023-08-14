f25292c0be5cae12ce27bbc50e892490c128362c
mahir118 <mahiranand11@gmail.com>
2023-08-14 12:25:57 +0530

    Searching in a sorted matrix

---
+++ Java DSA/SearchSortedMatrix.java
@@ -0,0 +1,25 @@
+public class SearchSortedMatrix {
+
+    public static void search (int matrix[][], int target) {
+        int col = 0, row = matrix.length - 1;
+        while (row >= 0 && col < matrix.length) {
+            if (matrix[row][col] > target) {
+                row--;
+            } else if (matrix[row][col] < target) {
+                col++;
+            } else {
+                System.out.print(row + " ");
+                System.out.print(col + " ");
+                break;
+            }
+        }
+    }
+    public static void main (String args[]) {
+        int matrix [][] = { {1,2,3,4},
+                            {5,6,7,8},
+                            {9,10,11,12},
+                            {13,14,15,16} };
+        search(matrix, 11);
+    }
+    
+}
