afb0b67cba66540c7f290d2756eb2e151c07fb57
Mahir Anand <104747526+mahir-anand@users.noreply.github.com>
2023-08-14 22:57:04 -0700

    Time: 0 ms (100.00%), Space: 44.2 MB (60.01%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,27 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int key = 0;
-        while (key < nums.length) {
-            if (key == nums[key]) {
-                key ++;
-            } else {
-                if (nums [key] == nums.length) {
-                    key ++;
-                } else {
-                    int temp = nums[ nums[key] ];
-                    nums [ nums[key] ] = nums[key];
-                    nums [key] = temp;
-                }
-            }
+        int N = nums.length;
+        int givenSum = 0;
+        int expectedSum = (N * (N+1)) / 2;
+        for (int i = 0 ; i < N ; i++) {
+            givenSum += nums[i];
         }
-        
-        
-        for (int i = 0 ; i < nums.length ; i++) {
-            if (i != nums[i]) {
-                return i;
-            } 
+        return expectedSum - givenSum;
         }
-        
-        return nums.length;
-    }
 }
