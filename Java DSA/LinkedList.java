21fcfd20817e68428d63011b5967cb3bf158fae1
mahir118 <mahiranand11@gmail.com>
2023-08-23 12:52:39 +0530

    Adding elements to the beginning and end

--- Java DSA/LinkedList.java
+++ Java DSA/LinkedList.java
@@ -26,12 +26,27 @@ public class LinkedList {
             head = newNode; // set the new node as the head node
         }
 
+        public void addLast (int data) {
+
+            Node newNode = new Node (data);
+
+            if (head == null) {
+                tail = head = newNode;
+                return;
+            }
+
+            tail.next = newNode;
+            tail = newNode;
+        }
+
     }
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
         ll.addFirst(1);
         ll.addFirst(2);
+        ll.addLast(3);
+        ll.addLast(4);
     }
     
 }
