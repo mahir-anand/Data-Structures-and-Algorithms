f3901f5c2ca5c8c879ee486c2ade65e3c88ddb0e
mahir118 <mahiranand11@gmail.com>
2023-08-25 08:05:07 +0530

    Initializing a Doubly Linked List

---
+++ Java DSA/DoubleLL.java
@@ -0,0 +1,112 @@
+public class DoubleLL {
+
+    public class Node {
+        int data;
+        Node next;
+        Node prev;
+
+        public Node (int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+    
+
+    //add
+    public void addFirst (int data) {
+        Node newNode = new Node (data);
+        size++;
+        if (head  == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    public void addLast (int data) {
+
+        Node newNode = new Node(data);
+        
+        if (size == 0) {
+            head = tail = newNode;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        newNode.prev = temp;
+        size++;
+    }
+
+    //print
+    public void print () {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    //remove
+    public int removeFirst() {
+        if (head == null) {
+            return Integer.MIN_VALUE;
+        }
+        if (head.next == null) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public int removeLast() {
+
+        if (head == null) {
+            return Integer.MIN_VALUE;
+        }
+
+        if (head.next == null) {
+            int val = head.data;
+            head = tail = null;
+            size --;
+            return val;
+        }
+
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        int val = temp.next.data;
+        temp.next = null;
+        return val;
+    }
+
+
+    public static void main(String[] args) {
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+        dll.print();
+        dll.addLast(4);
+        dll.print();
+    }
+
+}
