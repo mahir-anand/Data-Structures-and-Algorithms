053d546afe834625f6f5d25319eb5cee9b562b97
mahir118 <mahiranand11@gmail.com>
2023-09-14 07:39:04 -0700

    Push to the Bottom of a Stack

--- Java DSA/Stacks.java
+++ Java DSA/Stacks.java
@@ -1,57 +1,28 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Stacks {
     
-    static class node {
-        int data;
-        node next;
-        node(int data) {
-            this.data = data;
-            this.next = null;
+    public static void pushBottom (Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
         }
-    }
-
-    static class stack {
-        static node head = null;
         
-        public static boolean isEmpty() {
-            return head == null;
-        }
-
-        public static void push(int data) {
-            node temp = new node (data);
-            temp.next = head;
-            head = temp;
-        }
-
-        public static int pop() {
-            if (isEmpty()) {
-                return -1;
-            }
-            int temp = head.data;
-            head = head.next;
-            return temp;
-        }
-
-        public static int peek() {
-            if (isEmpty()) {
-                return -1;
-            }
-            return head.data;
-        }
+        int top = s.pop();
+        pushBottom(s, data);
+        s.push(top);
     }
-
-
 
     public static void main(String[] args) {
-        stack s = new stack();
-        s.push(1);
+        Stack<Integer> s = new Stack<>();
         s.push(2);
         s.push(3);
+        s.push(4);
 
+        pushBottom(s, 1);
+        
         while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+            System.out.println(s.pop());
         }
     
     }
