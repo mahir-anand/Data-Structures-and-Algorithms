4d3b17f54a6600461132c04ad2e023f15ed4b7b6
mahir118 <mahiranand11@gmail.com>
2023-09-14 08:14:24 -0700

    Reverse a Stack

--- Java DSA/Stacks.java
+++ Java DSA/Stacks.java
@@ -2,36 +2,57 @@ import java.util.*;
 
 public class Stacks {
     
-    // public static void pushBottom (Stack<Integer> s, int data) {
-    //     if (s.isEmpty()) {
-    //         s.push(data);
-    //         return;
-    //     }
+    public static void pushBottom (Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
         
-    //     int top = s.pop();
-    //     pushBottom(s, data);
-    //     s.push(top);
-    // }
-
-    public static String reverseString (String str) {
-        Stack<Character> s = new Stack<>();
-        int i = 0;
-        while (i < str.length()) {
-            s.push(str.charAt(i));
-            i++;
-        }
-
-        StringBuilder result = new StringBuilder();
-        while (!s.isEmpty()) {
-            char top = s.pop();
-            result.append(top);
-        }
-
-        return result.toString();
+        int top = s.pop();
+        pushBottom(s, data);
+        s.push(top);
     }
+
+    public static void reverse (Stack<Integer> s) {
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverse(s);
+        pushBottom(s,top);
+
+    }
+
+    public static void printStack (Stack<Integer> s) {
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+
+    // public static String reverseString (String str) {
+    //     Stack<Character> s = new Stack<>();
+    //     int i = 0;
+    //     while (i < str.length()) {
+    //         s.push(str.charAt(i));
+    //         i++;
+    //     }
+
+    //     StringBuilder result = new StringBuilder();
+    //     while (!s.isEmpty()) {
+    //         char top = s.pop();
+    //         result.append(top);
+    //     }
+
+    //     return result.toString();
+    // }
     public static void main(String[] args) {
-        String s = "abc";
-        System.out.println(reverseString(s));
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        reverse(s);
+        printStack(s);
     }
 
 }
