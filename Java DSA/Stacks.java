f82afa866d8b8a90f75e172b4a30d0f0366d8026
mahir118 <mahiranand11@gmail.com>
2023-09-16 10:30:32 -0700

    Largest Rectangle in Histogram

--- Java DSA/Stacks.java
+++ Java DSA/Stacks.java
@@ -8,32 +8,55 @@ public class Stacks {
         }
     }
 
-    public static boolean duplicateParenthesis (String str) {
-        Stack<Character> s = new Stack<>();
+    public static int maxRect (int[] height) {
+        Stack<Integer> s = new Stack<>();
 
-        for (int i = 0 ; i < str.length() ; i++) {
-            if (str.charAt(i) != ')') {
-                s.push(str.charAt(i));
-            } else {
-                int count = 0;
-                while (!s.isEmpty() && s.peek() != '(') {
-                    s.pop();
-                    count++;
-                }
-                if (count < 1) {
-                    return true;
-                }
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+
+        for (int i = 0 ; i < height.length ; i++) {
+            while (!s.isEmpty() && height[s.peek()] >= height[i]) {
                 s.pop();
             }
+            if (s.isEmpty()) {
+                left[i] = -1;
+            } else {
+                left[i] = s.peek();
+            }
+            s.push(i);
         }
-        return false;
-    }
 
+        while (!s.isEmpty()) {
+            s.pop();
+        }
+
+        for (int i = height.length - 1 ; i >= 0 ; i--) {
+            while (!s.isEmpty() && height[s.peek()] >= height[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                right[i] = height.length;
+            } else {
+                right[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0 ; i < height.length ; i++) {
+            int area = height[i] * (right[i] - left[i] - 1);
+            max = Math.max(max, area);
+        }
+
+        return max;
+
+    }
     
 
     public static void main(String[] args) {
-        String str = "";
-        System.out.println(duplicateParenthesis(str));
+        int[] height = {0,0,0,0,0};
+        System.out.println(maxRect(height));
     }
 
 }
