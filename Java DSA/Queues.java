0f3feb0c9e4809828e9f129778373b3c22c62e07
mahir118 <mahiranand11@gmail.com>
2023-09-19 07:49:07 -0700

    Reverse a Queue

--- Java DSA/Queues.java
+++ Java DSA/Queues.java
@@ -3,20 +3,14 @@ import java.util.LinkedList;
 
 public class Queues {
     
-    public static void interleave (Queue<Integer> q) {
-
-        Queue<Integer> first = new LinkedList<>();
-        
-        int size = q.size();
-        for (int i = 0 ; i < size / 2 ; i++) {
-            first.add(q.remove());
+    public static void reverse (Queue<Integer> q) {
+        Stack<Integer> stack = new Stack<>();
+        while (!q.isEmpty()) {
+            stack.push(q.remove());
         }
-
-        while (!first.isEmpty()) {
-            q.add(first.remove());
-            q.add(q.remove());
+        while (!stack.isEmpty()) {
+            q.add(stack.pop());
         }
-
     }
 
     public static void main(String[] args) {
@@ -29,7 +23,7 @@ public class Queues {
         q.add(5);
         q.add(6);
 
-        interleave(q);
+        reverse(q);
         for (int i : q) {
             System.out.print(i + " ");
         }
