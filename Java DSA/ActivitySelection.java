51b5fd986e5373a058052282e324fb995e7cdb8f
mahir118 <mahiranand11@gmail.com>
2023-11-21 08:11:44 -0800

    Sort per End Times

--- Java DSA/ActivitySelection.java
+++ Java DSA/ActivitySelection.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 
 public class ActivitySelection {
     
@@ -6,19 +8,28 @@ public class ActivitySelection {
         int[] start = {1,3,0,5,8,5};
         int[] end = {2,4,6,7,9,9};
         
+        int[][] matrix = new int[start.length][3];
+        for (int i = 0 ; i < start.length ; i++) {
+            matrix[i][0] = i;
+            matrix[i][1] = start[i];
+            matrix[i][2] = end[i];
+        }
+
+        Arrays.sort(matrix, Comparator.comparingDouble(o -> o[2]));
+
         int count = 0;
         ArrayList<Integer> ans = new ArrayList<>();
         
         // adding the first activity
         count = 1;
-        ans.add(0);
-        int endTime = end[0];
+        ans.add(matrix[0][0]);
+        int endTime = matrix[0][2];
 
         // looping for the remaining activities
         for (int i = 1 ; i < end.length ; i++) {
             if (start[i] >= endTime) {
-                ans.add(i);
-                endTime = end[i];
+                ans.add(matrix[i][0]);
+                endTime = matrix[i][2];
                 count++;
             }
         }
