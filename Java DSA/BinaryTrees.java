77e418eb175e3ed9762f74bc5385ec12a830ba41
mahir118 <mahiranand11@gmail.com>
2023-11-28 08:25:09 -0800

    Level Order Traversal

--- Java DSA/BinaryTrees.java
+++ Java DSA/BinaryTrees.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.*;
+
 public class BinaryTrees {
     
     static class Node {
@@ -55,13 +58,45 @@ public class BinaryTrees {
             System.out.println(root.data);
         }
 
+        public static void levelOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+            Queue<Node> q = new LinkedList<>();
+            q.add(root);
+            q.add(null);
+
+            while (!q.isEmpty()) {
+                Node curr = q.remove();
+                if (curr == null) {
+                    System.out.println();
+                    if (q.isEmpty()) {
+                        break;
+                    } 
+                    q.add(null);
+                } else {
+                    System.out.print(curr.data + " ");
+                    if (curr.left != null) {
+                        q.add(curr.left);
+                    } 
+                    if (curr.right != null) {
+                        q.add(curr.right);
+                    }
+                }
+                
+                
+                
+            }
+
+        }
+
     }
 
     public static void main(String[] args) {
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; 
         binaryTrees tree = new binaryTrees();  
         Node root = tree.buildTree(nodes);
-        tree.postOrder(root);
+        tree.levelOrder(root);
 
     }
 
