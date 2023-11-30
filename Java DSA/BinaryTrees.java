9bb0f13086f618d684deae69c83112404cf4b6eb
mahir118 <mahiranand11@gmail.com>
2023-11-30 08:22:07 -0800

    Diameter of a Tree - 1

--- Java DSA/BinaryTrees.java
+++ Java DSA/BinaryTrees.java
@@ -119,13 +119,29 @@ public class BinaryTrees {
 
         }
 
+        public static int diameter (Node root) {
+            if (root == null) {
+                return 0;
+            }
+
+            int ld = diameter(root.left);
+            int rd = diameter(root.right);
+            int lh = height(root.left);
+            int rh = height(root.right);
+
+            int self = lh + rh + 1;
+
+            return Math.max(self,Math.max(ld, rd));
+
+        }
+
     }
 
     public static void main(String[] args) {
         int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; 
         binaryTrees tree = new binaryTrees();  
         Node root = tree.buildTree(nodes);
-        System.out.println(tree.sumOfNodes(root));
+        System.out.println(tree.diameter(root));
 
     }
 
