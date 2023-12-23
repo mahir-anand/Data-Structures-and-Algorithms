01b1c8c133e24730a1c886188f504716d21b01ff
mahir118 <mahiranand11@gmail.com>
2023-12-22 19:54:48 -0800

    Delete from BST

--- Java DSA/BinarySearchTrees.java
+++ Java DSA/BinarySearchTrees.java
@@ -25,7 +25,43 @@ public class BinarySearchTrees {
 
         return root;
     }
+
+    public static Node delete(Node root, int val) {
+        if (root.data > val) {
+            root.left = delete(root.left,val);
+        } else if (root.data < val) {
+            root.right = delete(root.right,val);
+        } else {
+            //leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            //single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            //both children
+            Node is = inorderSucc(root.right);
+            root.data = is.data;
+            root.right = delete(root.right,is.data);
+
+        }
+
+        return root;
+
+    }
  
+    public static Node inorderSucc(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
     public static boolean search(Node root, int val) {
         
         if (root == null) {
@@ -51,19 +87,25 @@ public class BinarySearchTrees {
         }
         
         inorder(root.left);
-        System.out.print(root.data);
+        System.out.print(root.data + " ");
         inorder(root.right);
     }
 
     public static void main(String[] args) {
-        int[] values = {5,1,3,4,2,7};
+        int[] values = {8,5,3,1,4,6,10,11,14};
         Node root = null;
 
         for (int i = 0 ; i < values.length; i++) {
             root = insert(root,values[i]);
         }
 
-        System.out.println(search(root, 7));
+       inorder(root);
+       System.out.println();
+
+       delete(root, 5);
+       System.out.println();
+
+       inorder(root);
 
     }
 }
