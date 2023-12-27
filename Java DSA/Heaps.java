74d4355e4df5ba018357831b7a31c7b4cc97a766
mahir118 <mahiranand11@gmail.com>
2023-12-27 10:12:27 -0800

    Delete from Heap

--- Java DSA/Heaps.java
+++ Java DSA/Heaps.java
@@ -36,6 +36,48 @@ public class Heaps {
             return arr.get(0);
         }
 
+        private void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int min = i;
+            
+            if (left < arr.size() && left < arr.get(min)) {
+                min = left;
+            }
+
+            if (right < arr.size() && right < arr.get(min)) {
+                min = right;
+            }
+
+            if (min != i) {
+                int temp = arr.get(i);
+                arr.set(i,arr.get(min));
+                arr.set(min,temp);
+
+                heapify(min);
+            }
+
+        }
+
+        public int delete() {
+            int data = arr.get(0);
+            //swap
+            int temp = arr.get(0); 
+            arr.set(0,arr.get(arr.size()-1));
+            arr.set(arr.size()-1, temp);
+            
+            //delete last
+            arr.remove(arr.size()-1);
+
+            //heapfiy
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
+
     }
     
 
@@ -58,14 +100,17 @@ public class Heaps {
 
     public static void main(String[] args) {
         Heap h = new Heap();
-        h.add(2);
         h.add(3);
         h.add(4);
-        h.add(5);
-        h.add(10);
         h.add(1);
-        h.print();
-        System.out.println(h.peek());
+        h.add(10);
+        h.add(5);
+        
+        while (!h.isEmpty()) {
+            System.out.println(h.peek());
+            h.delete();
+        }
+
     }
     
 }
