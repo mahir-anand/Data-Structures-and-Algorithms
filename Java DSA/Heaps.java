4f4a99a9ad151c2181fa65a51a092928f994cc5b
mahir118 <mahiranand11@gmail.com>
2023-12-26 20:50:41 -0800

    Implement Priority Queues

---
+++ Java DSA/Heaps.java
@@ -0,0 +1,38 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Heaps {
+
+    static class Student implements Comparable<Student> {
+        
+        String name;
+        int rank;
+
+        public Student (String name, int rank) {
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override
+        public int compareTo (Student s2) {
+            return this.rank - s2.rank;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        PriorityQueue<Student> pq = new PriorityQueue<>(); 
+        //PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        //PriorityQueue<Integer> pq = new PriorityQueue<>();
+        pq.add(new Student("A", 4));
+        pq.add(new Student("B", 5));
+        pq.add(new Student("C", 2));
+        pq.add(new Student("D", 12));
+
+        while(!pq.isEmpty()) {
+            System.out.println(pq.peek().name + " --> " + pq.peek().rank);
+            pq.remove();
+        }
+    }
+    
+}
