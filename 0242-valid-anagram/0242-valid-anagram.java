3cc1046f0477f7551061742b58530717be62933e
Mahir Anand <104747526+mahir-anand@users.noreply.github.com>
2024-01-18 11:27:24 -0800

    Time: 18 ms (16.04%), Space: 44.9 MB (6.34%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,16 +5,25 @@ class Solution {
             return false;
         }
         
+        HashMap<Character, Integer> map = new HashMap<>();
+        
         for (int i = 0 ; i < s.length() ; i++) {
-            int index = t.indexOf(s.charAt(i));
-            if (index != -1) {
-                t = t.substring(0,index) + t.substring(index+1,t.length());
+            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+        
+        for (int i = 0 ; i < t.length() ; i++) {
+            if (map.get(t.charAt(i)) != null) {
+                if (map.get(t.charAt(i)) == 1) {
+                    map.remove(t.charAt(i));
+                } else {
+                    map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                }
             } else {
                 return false;
             }
         }
         
-        return t.isEmpty();
+        return map.isEmpty();
         
     }
 }
