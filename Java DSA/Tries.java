bb29914a68beb0757f22a6bcfd693bb35c255e9b
mahir118 <mahiranand11@gmail.com>
2024-03-03 13:54:28 +0100

    Insert in a Trie

--- Java DSA/Tries.java
+++ Java DSA/Tries.java
@@ -13,8 +13,25 @@ public class Tries {
 
     public static Node root = new Node();
 
-    public static void main(String[] args) {
+    public static void insert(String word) {
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
 
+        curr.eow = true;
+
+    }
+
+    public static void main(String[] args) {
+        String[] words = { "the", "a", "their" };
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
     }
 
 }
