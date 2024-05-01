d5eca5b852bd3ee078d97585d219d0e9c8ad3e74
mahir118 <mahiranand11@gmail.com>
2024-05-01 17:05:20 +0200

    Implement Breadth First Search

--- Java DSA/graphs.java
+++ Java DSA/graphs.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.LinkedList;
 
 public class graphs {
 
@@ -15,39 +16,64 @@ public class graphs {
 
     }
 
-    public static void main(String[] args) {
-        int V = 5;
-        // int[] arr = new arr[V];
-        ArrayList<Edge>[] graph = new ArrayList[V];
-        for (int i = 0; i < V; i++) {
+    static void createGraph(ArrayList<Edge> graph[]) {
+        for (int i = 0; i < graph.length; i++) {
             graph[i] = new ArrayList<>();
         }
 
-        // 0 vertex
-        graph[0].add(new Edge(0, 1, 5));
+        graph[0].add(new Edge(0, 1, 1));
+        graph[0].add(new Edge(0, 2, 1));
 
-        // 1 vertex
         graph[1].add(new Edge(1, 0, 1));
-        graph[1].add(new Edge(1, 2, 1));
-        graph[1].add(new Edge(1, 3, 3));
+        graph[1].add(new Edge(1, 3, 1));
 
-        // 2 vertex
-        graph[2].add(new Edge(2, 1, 1));
-        graph[2].add(new Edge(2, 3, 1));
-        graph[2].add(new Edge(2, 4, 2));
+        graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 4, 1));
 
-        // 3 vertex
-        graph[3].add(new Edge(3, 1, 3));
-        graph[3].add(new Edge(3, 2, 1));
+        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 4, 1));
+        graph[3].add(new Edge(3, 5, 1));
 
-        // 4 vertex
-        graph[4].add(new Edge(4, 2, 2));
+        graph[4].add(new Edge(4, 2, 1));
+        graph[4].add(new Edge(4, 3, 1));
+        graph[4].add(new Edge(4, 5, 1));
 
-        for (int i = 0; i < graph[2].size(); i++) {
-            Edge e = graph[2].get(i);
-            System.out.println(e.dest);
+        graph[5].add(new Edge(5, 3, 1));
+        graph[5].add(new Edge(5, 4, 1));
+        graph[5].add(new Edge(5, 6, 1));
+
+        graph[5].add(new Edge(6, 5, 1));
+
+    }
+
+    public static void bfs(ArrayList<Edge>[] graph) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] vis = new boolean[graph.length];
+        q.add(0); // source: 0
+
+        while (!q.isEmpty()) {
+
+            int curr = q.remove();
+
+            if (!vis[curr]) {
+                vis[curr] = true;
+                System.out.print(curr + " ");
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+
         }
 
+    }
+
+    public static void main(String[] args) {
+        int V = 7;
+        // int[] arr = new arr[V];
+        ArrayList<Edge>[] graph = new ArrayList[V];
+        createGraph(graph);
+        bfs(graph);
     }
 
 }
