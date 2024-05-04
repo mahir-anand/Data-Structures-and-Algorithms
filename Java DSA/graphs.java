c3cf17140e72352ae6983506f9ca17c8b8416dd2
mahir118 <mahiranand11@gmail.com>
2024-05-04 16:06:25 +0200

    Detect Cycle - Undirected Graph

--- Java DSA/graphs.java
+++ Java DSA/graphs.java
@@ -22,27 +22,19 @@ public class graphs {
         }
 
         graph[0].add(new Edge(0, 1, 1));
-        graph[0].add(new Edge(0, 2, 1));
+        // graph[0].add(new Edge(0, 2, 1));
+        graph[0].add(new Edge(0, 3, 1));
 
         graph[1].add(new Edge(1, 0, 1));
-        graph[1].add(new Edge(1, 3, 1));
+        graph[1].add(new Edge(1, 2, 1));
 
-        graph[2].add(new Edge(2, 0, 1));
-        graph[2].add(new Edge(2, 4, 1));
+        // graph[2].add(new Edge(2, 0, 1));
+        graph[2].add(new Edge(2, 1, 1));
 
-        graph[3].add(new Edge(3, 1, 1));
+        graph[3].add(new Edge(3, 0, 1));
         graph[3].add(new Edge(3, 4, 1));
-        graph[3].add(new Edge(3, 5, 1));
 
-        graph[4].add(new Edge(4, 2, 1));
         graph[4].add(new Edge(4, 3, 1));
-        graph[4].add(new Edge(4, 5, 1));
-
-        graph[5].add(new Edge(5, 3, 1));
-        graph[5].add(new Edge(5, 4, 1));
-        graph[5].add(new Edge(5, 6, 1));
-
-        graph[5].add(new Edge(6, 5, 1));
 
     }
 
@@ -91,7 +83,7 @@ public class graphs {
         for (int i = 0; i < graph[curr].size(); i++) {
             Edge e = graph[curr].get(i);
             if (!vis[e.dest]) {
-                dfs(graph, e.dest, vis);
+                dfsUtil(graph, e.dest, vis);
             }
         }
 
@@ -115,11 +107,48 @@ public class graphs {
 
     }
 
+    public static boolean detectCycle(ArrayList<Edge>[] graph) {
+        boolean[] vis = new boolean[graph.length];
+        for (int i = 0; i < graph.length; i++) {
+            if (!vis[i]) {
+                if (detectCycleUtil(graph, vis, i, -1)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+    }
+
+    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[] vis, int curr, int par) {
+        vis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            // case 3
+            if (!vis[e.dest]) {
+                if (detectCycleUtil(graph, vis, e.dest, curr)) {
+                    return true;
+                }
+            }
+            // case 1
+            else if (vis[e.dest] && e.dest != par) {
+                return true;
+            }
+            // case 2 -> do nothing so that we can continue
+        }
+
+        return false;
+
+    }
+
     public static void main(String[] args) {
-        int V = 7;
+        int V = 5;
         // int[] arr = new arr[V];
         ArrayList<Edge>[] graph = new ArrayList[V];
         createGraph(graph);
+        System.out.println(detectCycle(graph));
 
     }
 
