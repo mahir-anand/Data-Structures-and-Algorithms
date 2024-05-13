d1672581f0a4ce8d22fa08d24ce222eb7daf047a
mahir118 <mahiranand11@gmail.com>
2024-05-13 17:31:21 +0200

    Topological Sort BFS

--- Java DSA/graphs.java
+++ Java DSA/graphs.java
@@ -41,205 +41,245 @@ public class graphs {
 
     }
 
-    public static void bfs(ArrayList<Edge>[] graph) {
-        boolean[] vis = new boolean[graph.length];
+    // public static void bfs(ArrayList<Edge>[] graph) {
+    // boolean[] vis = new boolean[graph.length];
+    // for (int i = 0; i < graph.length; i++) {
+    // if (!vis[i]) {
+    // bfsUtil(graph, vis);
+    // }
+    // }
+    // }
+
+    // public static void bfsUtil(ArrayList<Edge>[] graph, boolean vis[]) {
+    // Queue<Integer> q = new LinkedList<>();
+    // q.add(0); // source: 0
+
+    // while (!q.isEmpty()) {
+
+    // int curr = q.remove();
+
+    // if (!vis[curr]) {
+    // vis[curr] = true;
+    // System.out.print(curr + " ");
+    // for (int i = 0; i < graph[curr].size(); i++) {
+    // Edge e = graph[curr].get(i);
+    // q.add(e.dest);
+    // }
+    // }
+
+    // }
+
+    // }
+
+    // public static void dfs(ArrayList<Edge>[] graph) {
+    // boolean[] vis = new boolean[graph.length];
+    // for (int i = 0; i < graph.length; i++) {
+    // dfsUtil(graph, i, vis);
+    // }
+    // }
+
+    // public static void dfsUtil(ArrayList<Edge>[] graph, int curr, boolean[] vis)
+    // {
+    // // visit
+    // System.out.print(curr + " ");
+    // vis[curr] = true;
+
+    // for (int i = 0; i < graph[curr].size(); i++) {
+    // Edge e = graph[curr].get(i);
+    // if (!vis[e.dest]) {
+    // dfsUtil(graph, e.dest, vis);
+    // }
+    // }
+
+    // }
+
+    // public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest,
+    // boolean[] vis) {
+
+    // if (src == dest) {
+    // return true;
+    // }
+
+    // vis[src] = true;
+    // for (int i = 0; i < graph[src].size(); i++) {
+    // Edge e = graph[src].get(i);
+    // if (!vis[e.dest] && hasPath(graph, e.dest, dest, vis)) {
+    // return true;
+    // }
+    // }
+
+    // return false;
+
+    // }
+
+    // public static boolean detectCycle(ArrayList<Edge>[] graph) {
+    // boolean[] vis = new boolean[graph.length];
+    // for (int i = 0; i < graph.length; i++) {
+    // if (!vis[i]) {
+    // if (detectCycleUtil(graph, vis, i, -1)) {
+    // return true;
+    // }
+    // }
+    // }
+
+    // return false;
+
+    // }
+
+    // public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[] vis,
+    // int curr, int par) {
+    // vis[curr] = true;
+
+    // for (int i = 0; i < graph[curr].size(); i++) {
+    // Edge e = graph[curr].get(i);
+    // // case 3
+    // if (!vis[e.dest]) {
+    // if (detectCycleUtil(graph, vis, e.dest, curr)) {
+    // return true;
+    // }
+    // }
+    // // case 1
+    // else if (vis[e.dest] && e.dest != par) {
+    // return true;
+    // }
+    // // case 2 -> do nothing so that we can continue
+    // }
+
+    // return false;
+
+    // }
+
+    // public static boolean isBipartite(ArrayList<Edge>[] graph) {
+    // int[] color = new int[graph.length];
+    // for (int i = 0; i < color.length; i++) {
+    // color[i] = -1; // no color
+    // }
+
+    // Queue<Integer> q = new LinkedList<>();
+
+    // for (int i = 0; i < graph.length; i++) {
+    // if (color[i] == -1) {
+    // q.add(i);
+    // color[i] = 0;
+    // while (!q.isEmpty()) {
+    // int curr = q.remove();
+    // for (int j = 0; j < graph[curr].size(); j++) {
+    // Edge e = graph[curr].get(j);
+    // if (color[e.dest] == -1) {
+    // int nextColor = color[curr] == 0 ? 1 : 0;
+    // color[e.dest] = nextColor;
+    // q.add(e.dest);
+    // } else if (color[e.dest] == color[curr]) {
+    // return false;
+    // }
+    // }
+    // }
+    // }
+    // }
+
+    // return true;
+
+    // }
+
+    // public static boolean isCycle(ArrayList<Edge>[] graph) {
+    // boolean[] vis = new boolean[graph.length];
+    // boolean[] stack = new boolean[graph.length];
+
+    // for (int i = 0; i < graph.length; i++) {
+    // if (!vis[i]) {
+    // if (isCycleUtil(graph, i, vis, stack)) {
+    // return true;
+    // }
+    // }
+    // }
+
+    // return false;
+
+    // }
+
+    // public static boolean isCycleUtil(ArrayList<Edge>[] graph, int cur, boolean[]
+    // vis, boolean[] stack) {
+    // vis[cur] = true;
+    // stack[cur] = true;
+
+    // for (int i = 0; i < graph[cur].size(); i++) {
+    // Edge e = graph[cur].get(i);
+    // if (stack[e.dest]) {
+    // return true;
+    // }
+
+    // if (!vis[e.dest] && isCycleUtil(graph, e.dest, vis, stack)) {
+    // return true;
+    // }
+    // }
+
+    // stack[cur] = false;
+
+    // return false;
+
+    // }
+
+    // public static void topSort(ArrayList<Edge>[] graph) {
+    // boolean[] vis = new boolean[graph.length];
+    // Stack<Integer> s = new Stack<>();
+
+    // for (int i = 0; i < graph.length; i++) {
+    // if (!vis[i]) {
+    // topSortUtil(graph, i, vis, s);
+    // }
+    // }
+
+    // while (!s.isEmpty()) {
+    // System.out.print(s.pop() + " ");
+    // }
+
+    // }
+
+    // public static void topSortUtil(ArrayList<Edge>[] graph, int cur, boolean[]
+    // vis, Stack<Integer> s) {
+    // vis[cur] = true;
+    // for (int i = 0; i < graph[cur].size(); i++) {
+    // Edge e = graph[cur].get(i);
+    // if (!vis[e.dest]) {
+    // topSortUtil(graph, e.dest, vis, s);
+    // }
+    // }
+    // s.push(cur);
+    // }
+
+    public static void calcIndeg(ArrayList<Edge>[] graph, int[] indeg) {
         for (int i = 0; i < graph.length; i++) {
-            if (!vis[i]) {
-                bfsUtil(graph, vis);
+            int v = i;
+            for (int j = 0; j < graph[v].size(); j++) {
+                Edge e = graph[v].get(j);
+                indeg[e.dest]++;
             }
         }
-    }
-
-    public static void bfsUtil(ArrayList<Edge>[] graph, boolean vis[]) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0); // source: 0
-
-        while (!q.isEmpty()) {
-
-            int curr = q.remove();
-
-            if (!vis[curr]) {
-                vis[curr] = true;
-                System.out.print(curr + " ");
-                for (int i = 0; i < graph[curr].size(); i++) {
-                    Edge e = graph[curr].get(i);
-                    q.add(e.dest);
-                }
-            }
-
-        }
-
-    }
-
-    public static void dfs(ArrayList<Edge>[] graph) {
-        boolean[] vis = new boolean[graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            dfsUtil(graph, i, vis);
-        }
-    }
-
-    public static void dfsUtil(ArrayList<Edge>[] graph, int curr, boolean[] vis) {
-        // visit
-        System.out.print(curr + " ");
-        vis[curr] = true;
-
-        for (int i = 0; i < graph[curr].size(); i++) {
-            Edge e = graph[curr].get(i);
-            if (!vis[e.dest]) {
-                dfsUtil(graph, e.dest, vis);
-            }
-        }
-
-    }
-
-    public static boolean hasPath(ArrayList<Edge>[] graph, int src, int dest, boolean[] vis) {
-
-        if (src == dest) {
-            return true;
-        }
-
-        vis[src] = true;
-        for (int i = 0; i < graph[src].size(); i++) {
-            Edge e = graph[src].get(i);
-            if (!vis[e.dest] && hasPath(graph, e.dest, dest, vis)) {
-                return true;
-            }
-        }
-
-        return false;
-
-    }
-
-    public static boolean detectCycle(ArrayList<Edge>[] graph) {
-        boolean[] vis = new boolean[graph.length];
-        for (int i = 0; i < graph.length; i++) {
-            if (!vis[i]) {
-                if (detectCycleUtil(graph, vis, i, -1)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-
-    }
-
-    public static boolean detectCycleUtil(ArrayList<Edge>[] graph, boolean[] vis, int curr, int par) {
-        vis[curr] = true;
-
-        for (int i = 0; i < graph[curr].size(); i++) {
-            Edge e = graph[curr].get(i);
-            // case 3
-            if (!vis[e.dest]) {
-                if (detectCycleUtil(graph, vis, e.dest, curr)) {
-                    return true;
-                }
-            }
-            // case 1
-            else if (vis[e.dest] && e.dest != par) {
-                return true;
-            }
-            // case 2 -> do nothing so that we can continue
-        }
-
-        return false;
-
-    }
-
-    public static boolean isBipartite(ArrayList<Edge>[] graph) {
-        int[] color = new int[graph.length];
-        for (int i = 0; i < color.length; i++) {
-            color[i] = -1; // no color
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < graph.length; i++) {
-            if (color[i] == -1) {
-                q.add(i);
-                color[i] = 0;
-                while (!q.isEmpty()) {
-                    int curr = q.remove();
-                    for (int j = 0; j < graph[curr].size(); j++) {
-                        Edge e = graph[curr].get(j);
-                        if (color[e.dest] == -1) {
-                            int nextColor = color[curr] == 0 ? 1 : 0;
-                            color[e.dest] = nextColor;
-                            q.add(e.dest);
-                        } else if (color[e.dest] == color[curr]) {
-                            return false;
-                        }
-                    }
-                }
-            }
-        }
-
-        return true;
-
-    }
-
-    public static boolean isCycle(ArrayList<Edge>[] graph) {
-        boolean[] vis = new boolean[graph.length];
-        boolean[] stack = new boolean[graph.length];
-
-        for (int i = 0; i < graph.length; i++) {
-            if (!vis[i]) {
-                if (isCycleUtil(graph, i, vis, stack)) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
-
-    }
-
-    public static boolean isCycleUtil(ArrayList<Edge>[] graph, int cur, boolean[] vis, boolean[] stack) {
-        vis[cur] = true;
-        stack[cur] = true;
-
-        for (int i = 0; i < graph[cur].size(); i++) {
-            Edge e = graph[cur].get(i);
-            if (stack[e.dest]) {
-                return true;
-            }
-
-            if (!vis[e.dest] && isCycleUtil(graph, e.dest, vis, stack)) {
-                return true;
-            }
-        }
-
-        stack[cur] = false;
-
-        return false;
-
     }
 
     public static void topSort(ArrayList<Edge>[] graph) {
-        boolean[] vis = new boolean[graph.length];
-        Stack<Integer> s = new Stack<>();
+        int[] indeg = new int[graph.length];
+        calcIndeg(graph, indeg);
+        Queue<Integer> q = new LinkedList<>();
 
-        for (int i = 0; i < graph.length; i++) {
-            if (!vis[i]) {
-                topSortUtil(graph, i, vis, s);
+        for (int i = 0; i < indeg.length; i++) {
+            if (indeg[i] == 0) {
+                q.add(i);
             }
         }
 
-        while (!s.isEmpty()) {
-            System.out.print(s.pop() + " ");
-        }
-
-    }
-
-    public static void topSortUtil(ArrayList<Edge>[] graph, int cur, boolean[] vis, Stack<Integer> s) {
-        vis[cur] = true;
-        for (int i = 0; i < graph[cur].size(); i++) {
-            Edge e = graph[cur].get(i);
-            if (!vis[e.dest]) {
-                topSortUtil(graph, e.dest, vis, s);
+        while (!q.isEmpty()) {
+            int cur = q.remove();
+            System.out.print(cur + " ");
+            for (int i = 0; i < graph[cur].size(); i++) {
+                Edge e = graph[cur].get(i);
+                indeg[e.dest]--;
+                if (indeg[e.dest] == 0) {
+                    q.add(e.dest);
+                }
             }
         }
-        s.push(cur);
+
     }
 
     public static void main(String[] args) {
