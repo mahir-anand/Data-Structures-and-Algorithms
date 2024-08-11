6b46f97f83326dcb75441a1ee6f3a7e00c82fc4a
mahir118 <mahiranand11@gmail.com>
2024-08-11 15:52:56 -0700

    Prims Algorithm

--- Java DSA/graphs.java
+++ Java DSA/graphs.java
@@ -21,31 +21,31 @@ public class graphs {
             graph[i] = new ArrayList<>();
         }
 
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 4));
+        graph[0].add(new Edge(0, 1, 10));
+        graph[0].add(new Edge(0, 2, 15));
+        graph[0].add(new Edge(0, 3, 30));
 
-        graph[1].add(new Edge(1, 2, -4));
+        graph[1].add(new Edge(1, 0, 10));
+        graph[1].add(new Edge(1, 3, 40));
 
-        graph[2].add(new Edge(2, 3, 2));
-        // graph[0].add(new Edge(0, 3, 1));
+        graph[2].add(new Edge(2, 0, 15));
+        graph[2].add(new Edge(2, 3, 30));
 
-        graph[3].add(new Edge(3, 4, 4));
-        // graph[1].add(new Edge(1, 3, 1));
-
-        graph[4].add(new Edge(4, 1, -1));
-
-    }
-
-    static void createGraph2(ArrayList<Edge> graph) {
-
-        graph.add(new Edge(0, 1, 2));
-        graph.add(new Edge(0, 2, 4));
-        graph.add(new Edge(1, 2, -4));
-        graph.add(new Edge(2, 3, 2));
-        graph.add(new Edge(3, 4, 4));
-        graph.add(new Edge(4, 1, -1));
+        graph[3].add(new Edge(3, 1, 40));
+        graph[3].add(new Edge(3, 2, 50));
 
     }
+
+    // static void createGraph2(ArrayList<Edge> graph) {
+
+    // graph.add(new Edge(0, 1, 2));
+    // graph.add(new Edge(0, 2, 4));
+    // graph.add(new Edge(1, 2, -4));
+    // graph.add(new Edge(2, 3, 2));
+    // graph.add(new Edge(3, 4, 4));
+    // graph.add(new Edge(4, 1, -1));
+
+    // }
 
     // public static void bfs(ArrayList<Edge>[] graph) {
     // boolean[] vis = new boolean[graph.length];
@@ -355,37 +355,77 @@ public class graphs {
 
     // }
 
-    public static void bellmanFord(ArrayList<Edge> graph, int src, int V) {
-        int[] dist = new int[V];
-        for (int i = 0; i < dist.length; i++) {
-            if (i != src) {
-                dist[i] = Integer.MAX_VALUE;
-            }
+    // public static void bellmanFord(ArrayList<Edge> graph, int src, int V) {
+    // int[] dist = new int[V];
+    // for (int i = 0; i < dist.length; i++) {
+    // if (i != src) {
+    // dist[i] = Integer.MAX_VALUE;
+    // }
+    // }
+
+    // for (int i = 0; i < V - 1; i++) {
+    // for (int j = 0; j < graph.size(); j++) {
+    // Edge e = graph.get(j);
+    // int u = e.src, v = e.dest, wt = e.wt;
+    // if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
+    // dist[v] = dist[u] + wt;
+    // }
+    // }
+    // }
+
+    // for (int i = 0; i < dist.length; i++) {
+    // System.out.print(dist[i] + " ");
+    // }
+
+    // }
+
+    static class Pair implements Comparable<Pair> {
+        int v;
+        int cost;
+
+        public Pair(int v, int c) {
+            this.v = v;
+            this.cost = c;
         }
 
-        for (int i = 0; i < V - 1; i++) {
-            for (int j = 0; j < graph.size(); j++) {
-                Edge e = graph.get(j);
-                int u = e.src, v = e.dest, wt = e.wt;
-                if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[v]) {
-                    dist[v] = dist[u] + wt;
-                }
-            }
-        }
-
-        for (int i = 0; i < dist.length; i++) {
-            System.out.print(dist[i] + " ");
+        @Override
+        public int compareTo(Pair p2) {
+            return this.cost - p2.cost;
         }
 
     }
 
+    public static void prims(ArrayList<Edge>[] graph) {
+
+        boolean vis[] = new boolean[graph.length];
+        PriorityQueue<Pair> pq = new PriorityQueue<>();
+        pq.add(new Pair(0, 0));
+        int finalCost = 0;
+
+        while (!pq.isEmpty()) {
+
+            Pair curr = pq.remove();
+
+            if (!vis[curr.v]) {
+                vis[curr.v] = true;
+                finalCost += curr.cost;
+
+                for (int i = 0; i < graph[curr.v].size(); i++) {
+                    Edge e = graph[curr.v].get(i);
+                    pq.add(new Pair(e.dest, e.wt));
+                }
+            }
+        }
+
+        System.out.println(finalCost);
+
+    }
+
     public static void main(String[] args) {
-        int V = 5;
-        // int[] arr = new arr[V];
-        // ArrayList<Edge>[] graph = new ArrayList[V];
-        ArrayList<Edge> graph = new ArrayList<>();
-        createGraph2(graph);
-        bellmanFord(graph, 0, V);
+        int V = 4;
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        prims(graph);
     }
 
 }
